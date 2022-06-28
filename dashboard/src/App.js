93774a3f763d9aaf85eb2a449ca8eb29b8482e89
Rahul2360 <rahulbansal2360@gmail.com>
2022-06-28 23:36:02 +0530

    navar boilerplate

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -11,31 +11,38 @@ const App = () => {
     <div>
       <BrowserRouter>
         <div className="flex relative dark:bg-main-dark-bg">
-          <div className="fixed right-4 bottom-4" style={{zIndex: '1000'}}>
+          <div className="fixed right-4 bottom-4" style={{ zIndex: '1000' }}>
             <TooltipComponent
               content="Settings"
               position="Top"
             >
               <button type='button' className='text-2xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white'
-                style={{background: 'blue', borderRadius:'50%'}}>
+                style={{ background: 'blue', borderRadius: '50%' }}>
                 <FiSettings></FiSettings>
               </button>
             </TooltipComponent>
           </div>
 
-          { activeMenu ? (
+          {activeMenu ? (
             <div className='w-72 fixed sidear dark:bg-secondary-dark-bg bg-white'>
               Sidebar
             </div>
-          ): (
+          ) : (
             <div className='w-0 dark:bg-secondary-dark-bg' >
               Sidebar w-0
             </div>
           )}
 
           <div className={
-            `dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72': 'flex-2'}`
+            `dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`
           }>
+
+            {/* Navbar */}
+
+            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-ful">
+              Navbar
+            </div>
+
 
           </div>
 
