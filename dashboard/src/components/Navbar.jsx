ce7aa942d927a59c8d244d2907ae6b28f3d19f79
Rahul2360 <rahulbansal2360@gmail.com>
2022-07-02 13:59:08 +0530

    Smnall icons issue resolved

--- dashboard/src/components/Navbar.jsx
+++ dashboard/src/components/Navbar.jsx
@@ -43,9 +43,8 @@ const Navbar = () => {
     >
       <button type="button" onClick={customFunc} style={{ color }}
         className="relative text-xl rounded-full p-3 hover:bg-light-gray">
-        <span className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2" style={{ backgroundColor: dotColor }}>
+        <span className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2" style={{ backgroundColor: dotColor }} />
           {icon}
-        </span>
       </button>
     </TooltipComponent>
   )
