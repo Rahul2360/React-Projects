dc8119be076b412e949108de1d0e57f69cf1b10f
Rahul2360 <rahulbansal2360@gmail.com>
2022-07-02 18:37:58 +0530

    Add expense and revenue section in ecommerce page

--- dashboard/src/pages/Ecommerce.jsx
+++ dashboard/src/pages/Ecommerce.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BsCurrencyDollar } from 'react-icons/bs';
 import { GoPrimitiveDot } from 'react-icons/go';
-import { Stacked, Pie, Button, Sparkline } from '../components';
+import { Stacked, Pie, Button, SparkLine } from '../components';
 
 import { earningData, SparklineAreaData, ecomPieChartData } from '../data/dummy';
 import { useStateContext } from '../contexts/ContextProvider';
@@ -9,6 +9,7 @@ import { useStateContext } from '../contexts/ContextProvider';
 const Ecommerce = () => {
   return (
     <div className="mt-12">
+
       <div className="flex flex-wrap lg:flex-nowrap justify-center">
         <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center">
           <div className="flex justify-between items-center">
@@ -30,14 +31,14 @@ const Ecommerce = () => {
 
         <div className="flex m-3 flex-wrap justify-center gap-11 items-center">
           {earningData.map((item) => (
-            <div 
+            <div
               key={item.title}
               className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg md:w-56 p-4 pt-9 rounded-2xl"
             >
               <button
-              type='button'
-              style={{color:item.iconColor, backgroundColor:item.iconBg}}
-              className="text-2xl opacity-0.9 rounded-full p-4 hover:drop-shadow-xl"
+                type='button'
+                style={{ color: item.iconColor, backgroundColor: item.iconBg }}
+                className="text-2xl opacity-0.9 rounded-full p-4 hover:drop-shadow-xl"
               >
                 {item.icon}
               </button>
@@ -56,6 +57,73 @@ const Ecommerce = () => {
             </div>
           ))}
         </div>
+      </div>
+
+      <div className="flex gap-10 flex-wrap justify-center">
+        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780">
+
+          <div className="flex justify-between">
+
+            <p className='font-semibold text-xl'>Revenue Updates</p>
+            <div className="flex item-center gap-4">
+              <p className="flex item-center gap-2 text-gray-600 hover:drop-shadow-xl">
+                <span>
+                  <GoPrimitiveDot />
+                </span>
+                <span>Expense</span>
+              </p>
+              <p className="flex item-center gap-2 text-green-400 hover:drop-shadow-xl">
+                <span>
+                  <GoPrimitiveDot />
+                </span>
+                <span>Budget</span>
+              </p>
+            </div>
+
+          </div>
+          
+          <div className="mt-10 flex gap-10 flex-wrap justify-center">
+            <div className="border-r-1 border-color m-4 pr-10">
+
+              <div>
+                <p>
+                  <span className='text-3xl font-semibold'>$62,134</span>
+                  <span className='p-1.5 hover:drop-shadow-xl cursor-pointer rounded-full text-white bg-green-400 ml-3 text-xs'>23%</span>
+                </p>
+                <p className='text-gray-500 mt-1'>
+                  Budget
+                </p>
+              </div>
+
+              <div className='mt-8'>
+                <p>
+                  <span className='text-3xl font-semibold'>$15,674</span>
+                  <span className='p-1.5 hover:drop-shadow-xl cursor-pointer rounded-full text-white bg-green-400 ml-3 text-xs'>23%</span>
+                </p>
+                <p className='text-gray-500 mt-1'>
+                  Expense
+                </p>
+              </div>
+
+              <div className="mt-5">
+                <SparkLine
+                  id="line-sparkline"
+                  currentColor="blue"
+                  type="line"
+                  height="80px"
+                  width="250px"
+                  data={SparklineAreaData}
+                  color="blue"
+                >
+
+                </SparkLine>
+              </div>
+
+            </div>
+          </div>
+
+        </div>
+
       </div>
     </div>
   )
