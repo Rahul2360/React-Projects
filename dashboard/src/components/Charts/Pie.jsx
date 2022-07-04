753093f42817b2d9bb7fb5d1e4fc0f21f37b1b58
Rahul2360 <rahulbansal2360@gmail.com>
2022-07-04 20:40:19 +0530

    Add pie and line chart from syncfusion

--- dashboard/src/components/Charts/Pie.jsx
+++ dashboard/src/components/Charts/Pie.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
 
-const Pie = () => {
+import { AccumulationChartComponent, AccumulationSeriesCollectionDirective, AccumulationSeriesDirective, PieSeries, Inject, AccumulationLegend, AccumulationDataLabel, AccumulationTooltip } from '@syncfusion/ej2-react-charts';
+import { useStateContext } from '../../contexts/ContextProvider';
+
+const PieChart = ({ id, data, height }) => {
+  const { currentMode } = useStateContext();
   return (
-    <div>Pie</div>
+    <AccumulationChartComponent
+      id={id}
+      legendSettings={{ visible: true, background: 'white' }}
+      height={height}
+      background={currentMode === 'Dark' ? '#33373E' : '#fff'}
+      tooltip={{ enable: true }}
+    >
+      <Inject services={[AccumulationLegend, PieSeries, AccumulationDataLabel, AccumulationTooltip]} />
+      <AccumulationSeriesCollectionDirective>
+        <AccumulationSeriesDirective dataSource={data} xName='x' yName='y' radius='80%' innerRadius="50%" explode
+          explodeOffset="10%"
+          explodeIndex={2} dataLabel={{
+            visible: true,
+            name: 'text',
+            position: 'Inside',
+            font: {
+              fontWeight: '600',
+              color: '#fff',
+            }
+          }}>
+        </AccumulationSeriesDirective>
+      </AccumulationSeriesCollectionDirective>
+    </AccumulationChartComponent>
   )
 }
 
-export default Pie
+export default PieChart
