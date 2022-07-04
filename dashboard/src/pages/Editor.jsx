815ebbb0c82490cf00b9173625627b077ab602ed
Rahul2360 <rahulbansal2360@gmail.com>
2022-07-04 20:38:56 +0530

    Add kanban, editor, calendar and color picker pages

--- dashboard/src/pages/Editor.jsx
+++ dashboard/src/pages/Editor.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { Header } from '../components';
 
+import { HtmlEditor, Image, Inject, Link, QuickToolbar, RichTextEditorComponent, Toolbar } from '@syncfusion/ej2-react-richtexteditor';
+
+import { EditorData } from '../data/dummy';
 const Editor = () => {
   return (
-    <div>Editor</div>
+    <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
+      <Header category="App" title="Editor"></Header>
+      <RichTextEditorComponent>
+        <EditorData/>
+        <Inject 
+        services={[HtmlEditor, Image, Toolbar, Link, QuickToolbar]}
+        />
+      </RichTextEditorComponent>
+    </div>
   )
 }
 
