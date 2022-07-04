e226cb60c547ddc393095b5dc26591b1e63309ef
Rahul2360 <rahulbansal2360@gmail.com>
2022-07-04 20:37:48 +0530

    Add area, bar,line, pie charts

--- dashboard/src/pages/Charts/Bar.jsx
+++ dashboard/src/pages/Charts/Bar.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import { ChartsHeader } from '../../components';
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, Legend, ColumnSeries, Tooltip, Category, DataLabel } from '@syncfusion/ej2-react-charts';
+
+import { barCustomSeries, barPrimaryXAxis, barPrimaryYAxis } from '../../data/dummy';
+import { useStateContext } from '../../contexts/ContextProvider';
 
 const Bar = () => {
+  const { currentMode } = useStateContext();
   return (
-    <div>Bar</div>
+    <div className='m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl'>
+      <ChartsHeader category="Bar" title="Olympic Medal Counts - RIO"></ChartsHeader>
+      <div className="w-full">
+        <ChartComponent
+          id='bar-chart'
+          height='420px'
+          primaryXAxis={barPrimaryXAxis}
+          primaryYAxis={barPrimaryYAxis}
+          chartArea={{
+            border: { width: 0 }
+          }}
+          tooltip={{ enable: true }}
+          background={currentMode === 'Dark' ? '#33373E' : '#fff'}
+          legendSettings={{ background: 'white' }}
+        >
+          <Inject services={[ColumnSeries, Legend, Tooltip, Category, DataLabel]} />
+          <SeriesCollectionDirective>
+            {barCustomSeries.map((item, index) => (
+              <SeriesDirective
+                key={index}
+                {...item}>
+              </SeriesDirective>
+            ))}
+          </SeriesCollectionDirective>
+        </ChartComponent>
+      </div>
+    </div>
   )
 }
 
